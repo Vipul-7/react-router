49bb21c86224673714352c347ccce875dc62afd4
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-02-17 11:51:02 +0530

    Add loader property for fetching data in route

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,35 +1,9 @@
-import { Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
-const DUMMY_EVENTS = [
-  {
-    id: "e1",
-    title: "Prakarsh 2023",
-    date: "14/03/2023",
-    description:
-      "Prakarsh is the national lavel event. Which is organized by our college student central committe",
-  },
-  {
-    id: "e2",
-    title: "Mood Indigo 2023",
-    date: "17/08/2023",
-    description:
-      "Mood Indigo is the IIT bombay cultural event. This is the one of the famous college event in the world.",
-  },
-];
+function EventsPage() {
+  const events = useLoaderData();
+  return <EventsList events={events} />;
+}
 
-const EventsPage = () => {
-  return (
-    <div>
-      <main>
-        <ul>
-          {DUMMY_EVENTS.map((evnt) => (
-            <li key={evnt.id}>
-              <Link to={evnt.id}>{evnt.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </main>
-    </div>
-  );
-};
 export default EventsPage;
