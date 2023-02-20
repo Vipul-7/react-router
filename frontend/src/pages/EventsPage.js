87cd9bbab39e670f432e8d9fee0d59ce6481eaa0
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-02-20 12:52:07 +0530

    Deferring Data Fetching with defer()

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,30 +1,56 @@
-import { json, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import { Await, defer, json, useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData();
-  // if(data.isError){
-  //   return <p>{data.message}</p>
-  // }
-  const events = data.events;
-  return <EventsList events={events} />;
+  // // if(data.isError){
+  // //   return <p>{data.message}</p>
+  // // }
+  // const events = data.events;
+  // return <EventsList events={events} />;
+
+  return (
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={data.events}>
+        {(loadedEvents) => 
+          <EventsList events={loadedEvents} />
+        }
+      </Await>
+    </Suspense>
+  );
 }
 
-export const loader = async () => {
+const loadEvents = async () => {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // return {isError:true , message : 'could not fetch the data'};
-    // throw {message : 'could not fetch the data'};
-    // throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
-    //   status: 500,
-    // });
     throw json({ message: "Could not fetch events" }, { status: 500 });
   } else {
-    // const resData = await response.json();
-    // return resData.events;
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+};
+
+export const loader = () => {
+  // const response = await fetch("http://localhost:8080/events");
+
+  // if (!response.ok) {
+  //   // return {isError:true , message : 'could not fetch the data'};
+  //   // throw {message : 'could not fetch the data'};
+  //   // throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
+  //   //   status: 500,
+  //   // });
+  //   throw json({ message: "Could not fetch events" }, { status: 500 });
+  // } else {
+  //   // const resData = await response.json();
+  //   // return resData.events;
+  //   return response;
+  // }
+
+  return defer({
+    events: loadEvents(),
+  });
 };
 
 export default EventsPage;
